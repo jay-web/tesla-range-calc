afea1d059aa834b05018bb9ad69429c38bfab5ad
jay-web <jay.developer12@gmail.com>
2022-01-10 12:31:09 +0530

    project starter

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logoUrl from "../../assets/logo.svg";
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <div className="header">
+            <img src={logoUrl} alt="Tesla" />
+        </div>
+    )
+}
+
+export default Header;
