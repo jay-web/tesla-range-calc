b3aaa0b8063c2a726f9df27fd10f0916e8f43b83
jay-web <jay.developer12@gmail.com>
2022-01-12 20:40:04 +0530

    created tesla counter reusable component to display speed and temperature, and update the same respectively

--- src/App.js
+++ src/App.js
@@ -4,10 +4,27 @@ import Header from "./components/Header/Header";
 import TeslaBattery from "./components/Container/TeslaBattery";
 
 function App() {
+  const counterDefaultVal = {
+    speed: {
+      title: "Speed",
+      unit: "mph",
+      step: 5,
+      min: 45,
+      max: 70
+    },
+    temperature: {
+      title: "Outside Temperature",
+      unit: "°",
+      step: 10,
+      min: -10,
+      max: 40
+    }
+  };
+
   return (
     <div className="App">
       <Header />
-      <TeslaBattery />
+      <TeslaBattery defaultValues={counterDefaultVal} />
     </div>
   );
 }
--- src/components/Container/TeslaBattery.js
+++ src/components/Container/TeslaBattery.js
@@ -1,44 +1,125 @@
-import React, {useState, useEffect } from "react";
-import {getModelData} from "../../services/BatteryService";
+import React, { useState, useEffect } from "react";
+import { getModelData } from "../../services/BatteryService";
 import "./TeslaBattery.css";
 import TeslaCar from "../TeslaCar/TeslaCar";
 import TeslaStats from "../TeslaStats/TeslaStats";
 import TeslaNotice from "../TeslaNotice/TeslaNotice";
+import TeslaCounter from "../TeslaCounter/TeslaCounter";
 
+const TeslaBattery = (props) => {
+  const [config, setConfig] = useState({
+    speed: 55,
+    temperature: 20,
+    climate: true,
+    wheels: 19,
+  });
+  const [carstats, setCarstats] = useState([]);
+  const [carModels, setCarModels] = useState([
+    "60",
+    "60D",
+    "75",
+    "75D",
+    "90D",
+    "P100D",
+  ]);
 
-const TeslaBattery = () => {
-    const [config, setConfig] = useState({speed:55, temperature: 20, climate: true,  wheels: 19});
-    const [carstats, setCarstats ] = useState([]);
-    const [carModels, setCarModels ] = useState(['60', '60D', '75', '75D', '90D', 'P100D']);
+  const calculateStats = (models, value) => {
+    const dataModels = getModelData();
 
-    const calculateStats = (models, value) => {
-        const dataModels = getModelData();
-        
-        return models.map((model) => {
-            const {speed, temperature, climate, wheels } =  value;
-            const miles = dataModels[model][wheels][climate ? 'on' : 'off'].speed[speed][temperature];
+    return models.map((model) => {
+      const { speed, temperature, climate, wheels } = value;
+      const miles =
+        dataModels[model][wheels][climate ? "on" : "off"].speed[speed][
+          temperature
+        ];
 
-            return {
-                model, 
-                miles
-            };
-        });
+      return {
+        model,
+        miles,
+      };
+    });
+  };
+
+  useEffect(() => {
+    setCarstats(calculateStats(carModels, config));
+  }, [carModels, config]);
+
+  const updateCounterState = (title, value) => {
+    const copyOfConfig = {...config};
+
+    title == 'Speed' ? copyOfConfig.speed = value : copyOfConfig.temperature = value;
+
+    setConfig(copyOfConfig);
+  }
+
+  const increment = (e, title) => {
+    e.preventDefault();
+    let currentValue, maxValue, step;
+    const {speed, temperature } = props.defaultValues;
+    if(title == "Speed"){
+        currentValue = config.speed;
+        maxValue = speed.max;
+        step = speed.step;
+    }else{
+        currentValue = config.temperature;
+        maxValue = temperature.max;
+        step = temperature.step;
     }
 
-    useEffect(() => {
-        setCarstats(calculateStats(carModels, config));
-    }, [carModels, config])
+    if(currentValue < maxValue){
+        const newValue = currentValue + step;
+        updateCounterState(title, newValue);
+    }
+  };
 
+  const decrement = (e, title) => {
+    e.preventDefault();
+    let currentValue, minValue, step;
+    const { speed, temperature } = props.defaultValues;
+    if(title == "Speed"){
+      currentValue = config.speed;
+      minValue = speed.min;
+      step = speed.step;
+    }else{
+      currentValue = config.temperature;
+      minValue = temperature.min;
+      step = temperature.step;
+    }
 
+    if(currentValue > minValue){
+      const newValue = currentValue - step;
+      updateCounterState(title, newValue);
 
-    return (
-        <form className="tesla-battery">
-            <h1>Range Per Charge</h1>
-            <TeslaCar wheelsize={config.wheels}/> 
-            <TeslaStats carstats={carstats}/>
-            <TeslaNotice />
-        </form>
-    )
-}
+    }
+      
+  };
+
+  return (
+    <form className="tesla-battery">
+      <h1>Range Per Charge</h1>
+      <TeslaCar wheelsize={config.wheels} />
+      <TeslaStats carstats={carstats} />
+      <div className="tesla-controls cf">
+        <TeslaCounter
+          currentValue={config.speed}
+          initValues={props.defaultValues.speed}
+          increment={increment}
+          decrement={decrement}
+        />
+        <div className="tesla-climate-container cf">
+        
+        <TeslaCounter
+          currentValue={config.temperature}
+          initValues={props.defaultValues.temperature}
+          increment={increment}
+          decrement={decrement}
+        />
+            
+            </div>
+      </div>
+      <TeslaNotice />
+    </form>
+  );
+};
 
 export default TeslaBattery;
